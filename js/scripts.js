ff6be1836b612bd1f2d60eeea9b65d5243a22f48
Skye Atwood <atwood.skye@gmail.com>
2016-10-28 10:33:02 -0700

    call back function, declare userInput

--- js/scripts.js
+++ js/scripts.js
@@ -4,34 +4,26 @@
 //else if((input%15)===0)
 
 
-
-for (i=1; i <=userInput; i++) {
-  if( i % 15 === 0 ) {
-    document.write("ping-pong");
-    console.log(i)
-  } else {
-    if( i % 5 === 0) {
-      document.write("pong");
+//userInput = 0
+//console.log(userInput)
+var userInput = function pp(input) {
+  for (i=1; i <= input; i++) {
+    if( i % 15 === 0 ) {
+      document.write("ping-pong");
+      //console.log(userInput);
     } else {
-      if(i % 3 === 0) {
-        document.write("ping");
+      if( i % 5 === 0) {
+        document.write("pong");
       } else {
-        document.write(i);
-      }
-    }
+        if(i % 3 === 0) {
+          document.write("ping");
+        } else {
+          document.write(i);
+        }
+      };
+    };
   };
-
 };
-
-
-
-
-
-
-
-
-
-
 
 //--------User Int Logic------------//
 $(document).ready(function() {
@@ -39,5 +31,6 @@ $(document).ready(function() {
     event.preventDefault();
 
     var userInput = $("input#input").val();
+    var pp =$pp(userInput);
   });
 });
