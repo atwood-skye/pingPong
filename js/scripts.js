16586a7d9db565eac118ae16df57484222eb0d3d
Skye Atwood <atwood.skye@gmail.com>
2016-10-28 11:57:10 -0700

    change output to display correctly in list form

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,26 @@
-//----------Business Logic---------//
-
-//else if((input%5)===0)
-//else if((input%15)===0)
-
-
-//userInput = 0
-//console.log(userInput)
-var userInput = function pp(input) {
-  for (i=1; i <= input; i++) {
-    if( i % 15 === 0 ) {
-      document.write("ping-pong");
-      //console.log(userInput);
-    } else {
-      if( i % 5 === 0) {
-        document.write("pong");
-      } else {
-        if(i % 3 === 0) {
-          document.write("ping");
-        } else {
-          document.write(i);
-        }
-      };
-    };
-  };
-};
 
 //--------User Int Logic------------//
 $(document).ready(function() {
   $("form").submit(function(event) {
-    event.preventDefault();
 
-    var userInput = $("input#input").val();
-    var pp =$pp(userInput);
-  });
+    //----------Business Logic---------//
+var userInput = $("input#input").val()
+  for (var i=1; i <= userInput; i++) {
+    if( i % 15 === 0 ) {
+      $("#answer").append('<li>'+"ping-pong"+'</li>');
+      console.log(userInput);
+    } else {
+      if( i % 5 === 0) {
+        $("#answer").append('<li>'+"pong"+'</li>');
+      } else {
+        if(i % 3 === 0) {
+        $("#answer").append('<li>'+"ping"+'</li>');
+        } else {
+          $("#answer").append('<li>'+i+'</li>');
+        }
+      };
+    };
+  };
+  event.preventDefault();
+});
 });
