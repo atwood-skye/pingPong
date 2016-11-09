8894c790e4c0f7ecad51e517846b47ff2ee163b1
Skye Atwood <atwood.skye@gmail.com>
2016-11-09 11:04:43 -0800

    seperate business and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,34 @@
+//-----Business Logic------//
+
+var numSearch = [];
+
+function searchNum(numberIn) {
+for (var i=1; i <= numberIn; i++) {
+  if( i % 15 === 0 ) {
+    numSearch.push("ping-pong");
+    //console.log(numSearch);
+  } else if ( i % 5 === 0) {
+      numSearch.push("pong");
+  } else if (i % 3 === 0) {
+      numSearch.push("ping");
+  } else {
+     numSearch.push(i);
+  }
+};
+};
+
+
 
 //--------User Int Logic------------//
 $(document).ready(function() {
   $("form").submit(function(event) {
+    event.preventDefault();
 
-    //----------Business Logic---------//
-var userInput = $("input#input").val()
-  for (var i=1; i <= userInput; i++) {
-    if( i % 15 === 0 ) {
-      $("#answer").append('<li>'+"ping-pong"+'</li>');
-      console.log(userInput);
-    } else {
-      if( i % 5 === 0) {
-        $("#answer").append('<li>'+"pong"+'</li>');
-      } else {
-        if(i % 3 === 0) {
-        $("#answer").append('<li>'+"ping"+'</li>');
-        } else {
-          $("#answer").append('<li>'+i+'</li>');
-        }
-      };
-    };
-  };
-  event.preventDefault();
-});
+    var formInput = parseInt($("input#input").val());
+    var numOutput = searchNum(formInput);
+
+    numSearch.forEach(function(output) {
+      $("#answer").append('<li>' + output + '</li>');
+    })
+  });
 });
